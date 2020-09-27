429510f698d4a4eaf786008bb88d4122075ff8a3
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-27 19:21:03 +0200

    new URL 2.53

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import YouTube from 'react-youtube';
 import axios from '../axios';
+import Youtube from 'react-youtube';
 
 //Base URL for Images Path --> "/4egSeI3qLvY22lHn4yIDrrU0ZNF.jpg"
 const imgBaseUrl = 'https://image.tmdb.org/t/p/w500';
@@ -7,6 +9,7 @@ const imgBaseUrl = 'https://image.tmdb.org/t/p/w500';
 function Row({ title, fetchUrl, isLargeRow }) {
   // State:
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   // UseEffect: When ROW Loads: Make the request
   useEffect(() => {
@@ -21,8 +24,35 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
     // This will run JUST ONCE when Component Loads (ROW) + Evertime Axios Link Changes
   }, [fetchUrl]);
+
   // console.log(movies); // Comprobamos dichos Destructured Arrays
 
+  // React Youtube:
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // URL
+      autoplay: 1,
+    },
+  };
+  // Functions:
+  const handleClick = () => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      // React-MovieTrailer --> Will search for the Title online.
+      movieTrailer(item.original_title || '')
+        // If Found-->
+        .then((url) => {
+          // Youtube Dummy URL : https://www.youtube.com/watch?v=XtMThy8QKqU&t=9658s
+          const urlParams = new URL(url).search;
+        }) //Not Found --> Log Error
+        .catch((error) => console.log(error));
+    }
+  };
+
+  // Main:
   return (
     <React.Fragment>
       <div className='row-container'>
@@ -31,6 +61,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
           {movies.map((item) => (
             <img
               key={item.id}
+              onClick={() => {
+                handleClick(item);
+              }}
               className={`row-poster-img ${isLargeRow && 'large-row'} `}
               src={`${imgBaseUrl}${
                 isLargeRow ? item.poster_path : item.backdrop_path
@@ -39,6 +72,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
             ></img>
           ))}
         </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}></YouTube>}
       </div>
     </React.Fragment>
   );
